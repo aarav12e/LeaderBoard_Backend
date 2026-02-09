98fecd0d694017c1f27f54b4fe8d3f775e867ab8
aarav12e <aarav12f@gmail.com>
2026-02-09 21:19:05 +0530

    corn job added in backend

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import studentRoutes from "./routes/studentRoutes.js";
+import job from "./config/cron.js";
 
-
-import cors from "cors";
 
 dotenv.config();
 connectDB();
@@ -13,8 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "OK" });
+});
 
 app.use("/api/students", studentRoutes);
 
+job.start();
 
 app.listen(process.env.PORT, () => console.log("Server running"));
